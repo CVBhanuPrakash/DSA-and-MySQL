d2109ded2668e58c3ac96a4b05ba79c0a482eea9
CHITLURU VENKATA BHANU PRAKASH <chitlurubhanuprakash03@gmail.com>
2023-07-11 23:52:46 +0530

    Time: 6 ms (28.11%), Space: 6.7 MB (56.83%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i=0; i<s.length(); i++){
-            if(s[i] == t[i]){
-                continue;
-            }
-            else return t[i];
-        }
-        return t[s.length()];
+        int xors=0;
+        for(auto it:s) xors=xors^it;
+        for(auto i:t) xors=xors^i;
+        return char(xors);
     }
 };
