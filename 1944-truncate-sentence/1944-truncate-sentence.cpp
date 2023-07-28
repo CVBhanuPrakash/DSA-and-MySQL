d7d17bc2704a597c22e9d599a1a0147d7becfc0a
CHITLURU VENKATA BHANU PRAKASH <chitlurubhanuprakash03@gmail.com>
2023-07-28 22:09:30 +0530

    Time: 3 ms (45.48%), Space: 6.6 MB (25.8%) - LeetHub

---
+++ 1944-truncate-sentence/1944-truncate-sentence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int n = s.length(), count = 0;
+        string ans;
+        for(int i=0; i<n; i++){
+            if(s[i] == ' ') count++;
+            if( count == k) break;
+            ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
