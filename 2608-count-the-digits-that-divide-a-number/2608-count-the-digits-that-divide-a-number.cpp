bf47d2510e6dc26704b517fefbf72c460d04a40b
CHITLURU VENKATA BHANU PRAKASH <chitlurubhanuprakash03@gmail.com>
2023-08-22 12:39:01 +0530

    Time: 0 ms (100%), Space: 6.1 MB (12.51%) - LeetHub

--- 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.cpp
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    int countDigits(int n) {
-        int t = n;
-        stack<int> s;
-        while(n>0){
-            s.push(n%10);
-            n = n/10;
+    int countDigits(int num) {
+        int n=num;
+        int cnt=0;
+        while(n){
+            int r=n%10;
+            n=n/10;
+            if(num%r==0)cnt++;
         }
-        int count = 0;
-        for(int i = s.size() - 1; i >=0; i--){
-            if((t%(s.top())) == 0){
-                count++;
-                s.pop();
-            }
-            else s.pop();
-        }
-    return count;
+        return cnt;
     }
 };
