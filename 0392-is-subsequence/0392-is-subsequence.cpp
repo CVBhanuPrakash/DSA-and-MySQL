0dd15df9e48a364c7b896e1bddbf3e1830a9bb7a
CHITLURU VENKATA BHANU PRAKASH <chitlurubhanuprakash03@gmail.com>
2023-09-22 11:53:28 +0530

    Time: 0 ms (100%), Space: 6.6 MB (17.6%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        for(int i=0; i<t.length(); i++){
+            if(t[i] == s[j]) j++;
+        }
+        if(j == s.length()) return true;
+        else return false;
+    }
+};
