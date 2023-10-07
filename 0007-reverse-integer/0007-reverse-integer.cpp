4b0a2d3a46cf2860fbb27c01072798381557dbe8
CHITLURU VENKATA BHANU PRAKASH <chitlurubhanuprakash03@gmail.com>
2023-10-07 15:11:22 +0530

    Time: 2 ms (38.84%), Space: 6.3 MB (26.91%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
-    int reverse(int x) {
-        long int ans = 0;
-        stack<int> s;
-        while(x != 0){
-            s.push(x%10);
+    long long reverse(int x) {
+        long long ans = 0;
+        while(x>0 || x<0){
+            ans = ans*10 + x%10;
             x = x/10;
-        }
-        long int one = 1;
-        while(!s.empty()){
-            ans = ans + s.top()*one;
-            s.pop();
-            one = one*10;
         }
         if(ans <= INT_MIN or ans >= INT_MAX) return 0;
         return ans;
