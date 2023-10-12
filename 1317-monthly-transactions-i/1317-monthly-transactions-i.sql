d51b7763a85107f4630c77571484a149b94c8f3b
CHITLURU VENKATA BHANU PRAKASH <chitlurubhanuprakash03@gmail.com>
2023-10-12 23:41:24 +0530

    Time: 1415 ms (20.98%), Space: 0B (100%) - LeetHub

---
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -0,0 +1,6 @@
+SELECT  SUBSTR(trans_date,1,7) as month, country, count(id) as trans_count, 
+    SUM(if(state = 'approved', 1, 0)) as approved_count, 
+    SUM(amount) as trans_total_amount, 
+    SUM(if(state = 'approved', amount, 0)) as approved_total_amount
+FROM Transactions
+GROUP BY month, country
