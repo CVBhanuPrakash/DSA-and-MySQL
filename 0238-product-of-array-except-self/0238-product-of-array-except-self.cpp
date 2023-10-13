db51dedec740eca250d2f7f7b5914e847a1d63f2
CHITLURU VENKATA BHANU PRAKASH <chitlurubhanuprakash03@gmail.com>
2023-10-13 16:09:25 +0530

    Time: 18 ms (63.44%), Space: 24.2 MB (73.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n =nums.size();
-        vector<int> pre(n, 1);
-        for(int i=1; i<n; i++){
-            pre[i] = pre[i-1] * nums[i-1];
-        }
-        vector<int> post(n, 1);
+        int n = nums.size();
+        vector<int> pp(n, 1);
+        for(int i=1; i<n; i++) pp[i] = pp[i-1] * nums[i-1];
+
+        int post = 1;
         for(int i=n-2; i>=0; i--){
-            post[i] = post[i+1] * nums[i+1];
+            post *= nums[i+1]; 
+            pp[i] = pp[i] * post;
+            
         }
-        for(int i=0; i<n; i++){
-            nums[i] = pre[i]*post[i];
-        }
-        return nums;
+        return pp;
+        // int n =nums.size();
+        // vector<int> pre(n, 1);
+        // for(int i=1; i<n; i++){
+        //     pre[i] = pre[i-1] * nums[i-1];
+        // }
+        // vector<int> post(n, 1);
+        // for(int i=n-2; i>=0; i--){
+        //     post[i] = post[i+1] * nums[i+1];
+        // }
+        // for(int i=0; i<n; i++){
+        //     nums[i] = pre[i]*post[i];
+        // }
+        // return nums;
     }
 };
